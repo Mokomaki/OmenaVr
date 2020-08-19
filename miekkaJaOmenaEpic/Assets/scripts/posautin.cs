4d27160e1a01769a23afe83562755cd33b4a8dfe
Mokomaki <grest.hyber@gmail.com>
2020-08-19 17:17:39 +0300

    Fixed shit up.

--- miekkaJaOmenaEpic/Assets/scripts/posautin.cs
+++ miekkaJaOmenaEpic/Assets/scripts/posautin.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Permissions;
 using UnityEngine;
 
 public class posautin : MonoBehaviour
@@ -8,14 +9,19 @@ public class posautin : MonoBehaviour
     public bool Smash;
     
 
+
     void Awake()
     {
         spawner = GameObject.FindGameObjectWithTag("spawner").GetComponent<Omena_spawner>();    
     }
 
+
     private void OnCollisionEnter(Collision otherCol)
     {
-
+        if(otherCol.gameObject.CompareTag("terrain"))
+        {
+            //Add decal on hit
+        }
         if (otherCol.gameObject.CompareTag("omena")|| otherCol.gameObject.CompareTag("paaryna"))
         {
             spawner.DestroyApple(otherCol.gameObject,Smash);
@@ -23,7 +29,6 @@ public class posautin : MonoBehaviour
         else
         if(otherCol.gameObject.CompareTag("JOUTSEN"))                       //if posautin hits a joutsen
         {
-            
             otherCol.gameObject.GetComponentInChildren<ParticleSystem>(true).gameObject.SetActive(true);
             Destroy(otherCol.gameObject.GetComponentInChildren<ParticleSystem>().gameObject, 10);               //taikaa
             for(int i = 0; i<otherCol.transform.childCount;i++)
@@ -36,7 +41,10 @@ public class posautin : MonoBehaviour
             otherCol.transform.DetachChildren();
             Destroy(otherCol.transform.gameObject);
         }
-
+        if(gameObject.CompareTag("Bullet"))
+        {
+            Destroy(gameObject);
+        }
     }
     private void OnCollisionExit(Collision collision)
     {
